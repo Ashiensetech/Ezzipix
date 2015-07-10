5b23c28d4867fc50f3101bf1aa699efb3d27be2f
mi_rafi <rafi@workspaceit.com>
2015-07-10 12:31:47 +0600

    minor change

---
+++ media.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 10-Jul-15
+ * Time: 12:28 PM
+ */
+
+class MediaController {
+    
+}
---
+++ view/image_gallary.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 10-Jul-15
+ * Time: 12:28 PM
+ */
