3fb6d55a8cfd3256d2264dcb84e8207520030c53
absiddiqueLive <absiddique.live@gmail.com>
2015-07-10 12:11:29 +0600

    model marge

--- Model/UserServiceDataModel.php
+++ Model/UserServiceDataModel.php
@@ -1,18 +1,26 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of UserServiceDataModel
- *
- * @author MI
- */
 class UserServiceData extends EzzipixModel {
-   function __construct() {
-       parent::__construct('user_service_data');
-   }
+    function __construct() {
+        parent::__construct('user_service_data');
+    }
+
+    function getAllMediaFileByUser_service_id($user_service_id) {
+        $query  = "SELECT $this->tableName.* FROM $this->tableName WHERE "
+                  . " user_service_id = $user_service_id";
+        $result = mysql_query($query);
+
+        return $this->getArrayData($result);
+    }
+
+    function getAllMediaFileByUid($u_id) {
+        $query  = "SELECT $this->tableName.* "
+                  . " FROM `$this->tableName`"
+                  . " INNER join user_service on user_service.id = $this->tableName.user_service_id "
+                  . " WHERE "
+                  . " user_service.u_id = " . $u_id;
+        $result = mysql_query($query);
+
+        return $this->getArrayData($result);
+    }
 }
