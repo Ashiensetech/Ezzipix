40fc3d38a0688a7b1053770e657ff86ba62465c7
absiddiqueLive <absiddique.live@gmail.com>
2015-09-18 18:06:21 +0600

    home update

--- view/home.php
+++ view/home.php
@@ -27,8 +27,8 @@ LAST UPDATE: 2015/01/05
     <!-- START Layerslider -->
     <!-- <section id="layerslider"style="width:100%; height:553px;">
         <div class="ls-slide" data-ls="transition2d:1; slidedelay:8000;">
-            <img src="<?php echo $this->baseUrl.'html_template/dist/image/layerslider/bg3.png';?>" class="ls-bg">
-            <img class="ls-l" style="top:90px;left:68%;" style="height: 200px;width: 200px" src="<?php echo $this->baseUrl.'html_template/dist/image/layerslider/layer/people2.png';?>" data-ls="delayin:1000; easingin:easeOutElastic;">
+            <img src="<?php echo $this->baseUrl . 'html_template/dist/image/layerslider/bg3.png'; ?>" class="ls-bg">
+            <img class="ls-l" style="top:90px;left:68%;" style="height: 200px;width: 200px" src="<?php echo $this->baseUrl . 'html_template/dist/image/layerslider/layer/people2.png'; ?>" data-ls="delayin:1000; easingin:easeOutElastic;">
             <h1 class="ls-l font-alt" style="top:110px;left:150px;" data-ls="offsetxin:0;durationin:2000;delayin:1500;easingin:easeOutElastic;rotatexin:-90;transformoriginin:50% top 0;offsetxout:-200;durationout:1000;">
                 Welcome To <span class="text-primary">Ezzipix</span>
             </h1>
@@ -42,7 +42,7 @@ LAST UPDATE: 2015/01/05
             </p>
         </div>
         <div class="ls-slide" data-ls="transition2d:1; slidedelay:8000;">
-            <img src="<?php echo $this->baseUrl.'html_template/dist/image/layerslider/bg2.png';?>" class="ls-bg" alt="Slide background">
+            <img src="<?php echo $this->baseUrl . 'html_template/dist/image/layerslider/bg2.png'; ?>" class="ls-bg" alt="Slide background">
             <h4 class="ls-l text-default text-right" style="top:120px;left:65%;width:550px;" data-ls="easingin:easeOutElastic; delayin:0;">
                 Fully Responsive &amp; Clean Design
             </h4>
@@ -59,13 +59,13 @@ LAST UPDATE: 2015/01/05
                     Learn More <i class="ico-angle-right ml5"></i>
                 </a>
             </p>
-            <img class="ls-l" style="top:80px;left:250px;" src="<?php echo $this->baseUrl.'html_template/dist/image/layerslider/layer/iphone-5s.png';?>" data-ls="delayin:2000; easingin:easeOutElastic;">
+            <img class="ls-l" style="top:80px;left:250px;" src="<?php echo $this->baseUrl . 'html_template/dist/image/layerslider/layer/iphone-5s.png'; ?>" data-ls="delayin:2000; easingin:easeOutElastic;">
         </div>
     </section> -->
     <!--/ END Layerslider -->
 
-    <section id="photo-plugin">
-
+    <section id="photo-plugin" class="text-center bgcolor-white">
+        <img src="<?php echo $this->baseUrl . "html_template/dist/image/loading/spinner@2x.gif"; ?>" id="photo-plugin-loder"/>
     </section>
 
     <!-- START Call To Action Section -->
@@ -75,9 +75,9 @@ LAST UPDATE: 2015/01/05
                 <h3 class="font-alt text-white nm mt3">Ezzipix sed ut perspiciatis unde omnis iste natus . </h3>
             </div>
             <div class="mb15 visible-xs"></div>
-<!--            <div class="col-sm-3 clearfix">-->
-<!--                <a href="javascript:void(0);" class="btn btn-outline btn-default text-white pull-right semibold">CALL TO ACTION</a>-->
-<!--            </div>-->
+            <!--            <div class="col-sm-3 clearfix">-->
+            <!--                <a href="javascript:void(0);" class="btn btn-outline btn-default text-white pull-right semibold">CALL TO ACTION</a>-->
+            <!--            </div>-->
         </div>
     </section>
     <!-- END Call To Action Section -->
@@ -90,6 +90,7 @@ LAST UPDATE: 2015/01/05
                 <div class="col-md-12">
                     <div class="section-header text-center">
                         <h1 class="section-title font-alt mb25">Awesome Features</h1>
+
                         <div class="row">
                             <div class="col-md-8 col-md-offset-2">
                                 <h4 class="thin text-muted text-center">
@@ -109,9 +110,12 @@ LAST UPDATE: 2015/01/05
             <div class="row">
                 <div class="col-md-4">
                     <div class="table-layout animation" data-toggle="waypoints" data-showanim="fadeInRight" data-trigger-once="true">
-                        <div class="col-xs-2 valign-top"><img src="<?php echo $this->baseUrl.'html_template/dist/image/icons/responsivewebdesign.png'; ?>" width="100%" alt=""></div>
+                        <div class="col-xs-2 valign-top">
+                            <img src="<?php echo $this->baseUrl . 'html_template/dist/image/icons/responsivewebdesign.png'; ?>" width="100%" alt="">
+                        </div>
                         <div class="col-xs-19 pl15">
                             <h4 class="font-alt">Responsive Design</h4>
+
                             <p class="nm">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                 tempor incididunt.</p>
                         </div>
@@ -119,20 +123,28 @@ LAST UPDATE: 2015/01/05
                 </div>
                 <div class="col-md-4">
                     <div class="table-layout animation" data-toggle="waypoints" data-showanim="fadeInRight" data-trigger-once="true">
-                        <div class="col-xs-2 valign-top"><img src="<?php echo $this->baseUrl.'html_template/dist/image/icons/seoplanning.png';?>" width="100%" alt=""></div>
+                        <div class="col-xs-2 valign-top">
+                            <img src="<?php echo $this->baseUrl . 'html_template/dist/image/icons/seoplanning.png'; ?>" width="100%" alt="">
+                        </div>
                         <div class="col-xs-10 pl15">
                             <h4 class="font-alt">SEO Optimize</h4>
-                            <p class="nm">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+
+                            <p class="nm">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                aliquip ex ea commodo
                                 consequat.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="table-layout animation" data-toggle="waypoints" data-showanim="fadeInRight" data-trigger-once="true">
-                        <div class="col-xs-2 valign-top"><img src="<?php echo $this->baseUrl.'html_template/dist/image/icons/branddevelopment.png';?>" width="100%" alt=""></div>
+                        <div class="col-xs-2 valign-top">
+                            <img src="<?php echo $this->baseUrl . 'html_template/dist/image/icons/branddevelopment.png'; ?>" width="100%" alt="">
+                        </div>
                         <div class="col-xs-10 pl15">
                             <h4 class="font-alt">Blazing Fast</h4>
-                            <p class="nm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                            <p class="nm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                deserunt mollit anim id est laborum.</p>
                         </div>
                     </div>
                 </div>
@@ -143,9 +155,12 @@ LAST UPDATE: 2015/01/05
             <div class="row">
                 <div class="col-md-4">
                     <div class="table-layout animation mb0" data-toggle="waypoints" data-showanim="fadeInLeft" data-trigger-once="true">
-                        <div class="col-xs-2 valign-top"><img src="<?php echo $this->baseUrl.'html_template/dist/image/icons/affiliatemarketing.png';?>" width="100%" alt=""></div>
+                        <div class="col-xs-2 valign-top">
+                            <img src="<?php echo $this->baseUrl . 'html_template/dist/image/icons/affiliatemarketing.png'; ?>" width="100%" alt="">
+                        </div>
                         <div class="col-xs-19 pl15">
                             <h4 class="font-alt">Worth Every Peny</h4>
+
                             <p class="nm">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                 tempor incididunt.</p>
                         </div>
@@ -154,10 +169,14 @@ LAST UPDATE: 2015/01/05
                 <div class="mb15 visible-xs visible-sm"></div>
                 <div class="col-md-4">
                     <div class="table-layout animation mb0" data-toggle="waypoints" data-showanim="fadeInLeft" data-trigger-once="true">
-                        <div class="col-xs-2 valign-top"><img src="<?php echo $this->baseUrl.'html_template/dist/image/icons/targetaudience.png';?>" width="100%" alt=""></div>
+                        <div class="col-xs-2 valign-top">
+                            <img src="<?php echo $this->baseUrl . 'html_template/dist/image/icons/targetaudience.png'; ?>" width="100%" alt="">
+                        </div>
                         <div class="col-xs-10 pl15">
                             <h4 class="font-alt">Build With You In Mind</h4>
-                            <p class="nm">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+
+                            <p class="nm">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                aliquip ex ea commodo
                                 consequat.</p>
                         </div>
                     </div>
@@ -165,10 +184,14 @@ LAST UPDATE: 2015/01/05
                 <div class="mb15 visible-xs visible-sm"></div>
                 <div class="col-md-4">
                     <div class="table-layout animation mb0" data-toggle="waypoints" data-showanim="fadeInLeft" data-trigger-once="true">
-                        <div class="col-xs-2 valign-top"><img src="<?php echo $this->baseUrl.'html_template/dist/image/icons/customcoding.png';?>" width="100%" alt=""></div>
+                        <div class="col-xs-2 valign-top">
+                            <img src="<?php echo $this->baseUrl . 'html_template/dist/image/icons/customcoding.png'; ?>" width="100%" alt="">
+                        </div>
                         <div class="col-xs-10 pl15">
                             <h4 class="font-alt">Clean Code</h4>
-                            <p class="nm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                            <p class="nm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                deserunt mollit anim id est laborum.</p>
                         </div>
                     </div>
                 </div>
@@ -210,7 +233,7 @@ LAST UPDATE: 2015/01/05
                                     </div>
                                 </div>
                                 <!--/ toolbar overlay -->
-                                <img data-toggle="unveil" src="<?php echo $this->baseUrl.'html_template/dist/image/background/blog/placeholder.jpg';?>" data-src="<?php echo $this->baseUrl.'html_template/dist/image/background/blog/blog1.jpg'?>" alt="Photo" height="200px">
+                                <img data-toggle="unveil" src="<?php echo $this->baseUrl . 'html_template/dist/image/background/blog/placeholder.jpg'; ?>" data-src="<?php echo $this->baseUrl . 'html_template/dist/image/background/blog/blog1.jpg' ?>" alt="Photo" height="200px">
                             </div>
                             <!--/ media -->
                         </header>
@@ -251,7 +274,7 @@ LAST UPDATE: 2015/01/05
                                     </div>
                                 </div>
                                 <!--/ toolbar overlay -->
-                                <img data-toggle="unveil" src="<?php echo $this->baseUrl.'html_template/dist/image/background/blog/placeholder.jpg';?>" data-src="<?php echo $this->baseUrl.'html_template/dist/image/background/blog/blog2.jpg';?>" alt="Photo" height="200px">
+                                <img data-toggle="unveil" src="<?php echo $this->baseUrl . 'html_template/dist/image/background/blog/placeholder.jpg'; ?>" data-src="<?php echo $this->baseUrl . 'html_template/dist/image/background/blog/blog2.jpg'; ?>" alt="Photo" height="200px">
                             </div>
                             <!--/ media -->
                         </header>
@@ -292,7 +315,7 @@ LAST UPDATE: 2015/01/05
                                     </div>
                                 </div>
                                 <!--/ toolbar overlay -->
-                                <img data-toggle="unveil" src="<?php echo $this->baseUrl.'html_template/dist/image/background/blog/placeholder.jpg';?>" data-src="<?php echo $this->baseUrl.'html_template/dist/image/background/blog/blog3.jpg';?>" alt="Photo" height="200px">
+                                <img data-toggle="unveil" src="<?php echo $this->baseUrl . 'html_template/dist/image/background/blog/placeholder.jpg'; ?>" data-src="<?php echo $this->baseUrl . 'html_template/dist/image/background/blog/blog3.jpg'; ?>" alt="Photo" height="200px">
                             </div>
                             <!--/ media -->
                         </header>
@@ -300,7 +323,8 @@ LAST UPDATE: 2015/01/05
                         <!-- Content -->
                         <section class="pa20">
                             <!-- heading -->
-                            <h4 class="mt0 ellipsis">Vivamus nibh dolor, nonummy ac, feugiat non, lobortis quis, pede.</h4>
+                            <h4 class="mt0 ellipsis">Vivamus nibh dolor, nonummy ac, feugiat non, lobortis quis,
+                                pede.</h4>
                             <!--/ heading -->
                             <!-- meta -->
                             <p class="meta nm">
@@ -339,7 +363,7 @@ LAST UPDATE: 2015/01/05
 <?php include_once 'partial/core_script.php' ?>
 <!--/ Application and vendor script : mandatory -->
 
-<script src="<?php echo $this->baseUrl. "js/printio/pio.latest.v2.js"; ?>"></script>
+<script src="<?php echo $this->baseUrl . "js/printio/pio.latest.v2.js"; ?>"></script>
 <script type="text/javascript">
 
     var imgJobjArray = JSON.parse($("#allImg").val())
@@ -361,28 +385,49 @@ LAST UPDATE: 2015/01/05
 
 </script>
 <!-- Plugins and page level script : optional -->
-<script type="text/javascript" src="<?php echo $this->baseUrl.'html_template/dist/plugins/smoothscroll/js/smoothscroll.js';?>"></script>
-<script type="text/javascript" src="<?php echo $this->baseUrl.'html_template/dist/plugins/owl/js/owl.carousel.js';?>"></script>
-<script type="text/javascript" src="<?php echo $this->baseUrl.'html_template/dist/plugins/layerslider/js/greensock.js';?>"></script>
-<script type="text/javascript" src="<?php echo $this->baseUrl.'html_template/dist/plugins/layerslider/js/layerslider.transitions.js';?>"></script>
-<script type="text/javascript" src="<?php echo $this->baseUrl.'html_template/dist/plugins/layerslider/js/layerslider.kreaturamedia.jquery.js';?>"></script>
-<script type="text/javascript" src="<?php echo $this->baseUrl.'html_template/dist/javascript/frontend/home/home-v1.js';?>"></script>
+<script type="text/javascript" src="<?php echo $this->baseUrl . 'html_template/dist/plugins/smoothscroll/js/smoothscroll.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo $this->baseUrl . 'html_template/dist/plugins/owl/js/owl.carousel.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo $this->baseUrl . 'html_template/dist/plugins/layerslider/js/greensock.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo $this->baseUrl . 'html_template/dist/plugins/layerslider/js/layerslider.transitions.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo $this->baseUrl . 'html_template/dist/plugins/layerslider/js/layerslider.kreaturamedia.jquery.js'; ?>"></script>
+<script type="text/javascript" src="<?php echo $this->baseUrl . 'html_template/dist/javascript/frontend/home/home-v1.js'; ?>"></script>
 <!--/ Plugins and page level script : optional -->
 <!--/ END JAVASCRIPT SECTION -->
 
 <script type="text/javascript">
-    jQuery(document).ready(function(){
+    jQuery(document).ready(function () {
         var el = document.getElementById('photo-plugin');
+
         PIO.open({
-            recipeId:"6cfb4f30-34c7-4cf6-9490-f51925650811",
-            url:"https://widget.print.io/widget/",
-            embedInElement:el,
-            style:{
-                showHeader:false
+            recipeId: "6cfb4f30-34c7-4cf6-9490-f51925650811",
+            url: "https://widget.print.io/widget/",
+            embedInElement: el,
+            style: {
+                showHeader: false
+            },
+            iframeStyles: {
+                width: "100%",
+                "max-width": "inherit"
+            },
+            fns: {
+                //callback for when the widget cart/order changes
+                onCartChange: function (cart) {
+
+                },
+                //callback on widget close
+                onClose: function () {
+
+                },
+                //callback on widget open
+                onOpen: function () {
+                    $("#photo-plugin-loder").css("display", "none");
+                }
             }
         });
 
-        $("iframe").css({"max-width": "100%", "width": "100%"});
+        //$("#photo-plugin-loder").css("display", "none");
+
+        //$("iframe").css({"max-width": "100%", "width": "100%"});
     });
 </script>
 
