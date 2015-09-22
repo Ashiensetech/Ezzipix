ca2cc81ee7084de478d045047eae9ebbf8fd5b40
absiddiqueLive <absiddique.live@gmail.com>
2015-09-22 14:50:18 +0600

    update

--- view/partial/script/facebook_script.php
+++ view/partial/script/facebook_script.php
@@ -8,18 +8,16 @@
             xfbml: true,
             version: 'v2.4'
         });
+        fbLogin();
     };
 
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+    }(document));
 
     function opacityFreeMe(elem){
         $(elem).css("opacity","1");
