51ac0ab0f373366a409ad34e10b64898c9bb956a
tomal2514 <mtit_tomal@hotmail.com>
2016-03-18 14:07:42 +0600

    Mail check 7

--- signup.php
+++ signup.php
@@ -78,9 +78,9 @@ class SignupController extends EzzipixController {
             $data  = $confirm->setToken($email);
             /*$headers =  'MIME-Version: 1.0' . "\r\n";
             $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";*/
-            $headers = "Reply-To: admin support@ezeepix.com \r\n";
-            $headers .= "Return-Path: admin support@ezeepix.com \r\n";
-            $headers .= "From: admin support@ezeepix.com \r\n";
+            $headers = "Reply-To: support@ezeepix.com \r\n";
+            $headers .= "Return-Path: support@ezeepix.com \r\n";
+            $headers .= "From: support@ezeepix.com \r\n";
             $headers .= "Organization: Ezeepix\r\n";
             $headers .= "Content-Type: text/plain\r\n";
 
